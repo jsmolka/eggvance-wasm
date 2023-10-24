71b310aa9fad8577d2080dc6ae04aa7eba7b6a6d
jsmolka <jsmolka@pm.me>
2023-10-24 16:20:03 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col items-center gap-4 w-full max-w-screen-xl p-4">
     <canvas id="canvas" class="w-full aspect-[3/2] pointer-events-none select-none" />
-    <input hidden ref="inputGba" type="file" accept=".gba" @change="eggvance.loadGba()" />
-    <input hidden ref="inputSav" type="file" accept=".sav" @change="eggvance.loadSav()" />
+    <input hidden ref="gba" type="file" accept=".gba" @change="eggvance.loadGba()" />
+    <input hidden ref="sav" type="file" accept=".sav" @change="eggvance.loadSav()" />
     <div class="flex gap-4">
-      <Button :disabled="disabled" @click="inputGba.click()">Load ROM</Button>
-      <Button :disabled="disabled" @click="inputSav.click()">Load save</Button>
+      <Button :disabled="disabled" @click="gba.click()">Load ROM</Button>
+      <Button :disabled="disabled" @click="sav.click()">Load save</Button>
       <Button :disabled="disabled" @click="eggvance.loadDemo()">Load demo</Button>
       <Button @click="controls = !controls">
         {{ controls ? 'Hide controls' : 'Show controls' }}
@@ -19,33 +19,33 @@
 import { computed, onMounted, ref } from 'vue';
 import { Button, Controls } from './components';
 
-const inputGba = ref();
-const inputSav = ref();
 const controls = ref(false);
 const disabled = ref(false);
 const eggvance = computed(() => window.Module);
 
-const readFile = async (input) => {
+const read = async (file) => {
   disabled.value = true;
   try {
     return await new Promise((resolve) => {
       const reader = new FileReader();
       reader.onload = () => resolve(new Uint8Array(reader.result));
-      reader.readAsArrayBuffer(input.files[0]);
-      input.value = '';
+      reader.readAsArrayBuffer(file);
     });
   } finally {
     disabled.value = false;
   }
 };
 
-let fileId = 0;
+let id = 0;
 
-const writeFile = (data, ext) => {
-  const name = `${fileId++}.${ext}`;
+const write = (data, extension) => {
+  const name = `${id++}.${extension}`;
   FS.writeFile(name, data);
   return name;
 };
+
+const gba = ref();
+const sav = ref();
 
 window.Module = {
   onRuntimeInitialized() {
@@ -53,27 +53,27 @@ window.Module = {
   },
 
   async loadGba() {
-    const data = await readFile(inputGba.value);
-    const name = writeFile(data, 'gba');
+    const data = await read(gba.value.files[0]);
+    const name = write(data, 'gba');
     this.eggvanceLoadGba(name);
   },
 
   async loadSav() {
-    const data = await readFile(inputSav.value);
-    const name = writeFile(data, 'sav');
+    const data = await read(sav.value.files[0]);
+    const name = write(data, 'sav');
     this.eggvanceLoadSav(name);
   },
 
   async loadDemo() {
     disabled.value = true;
     try {
-      const response = await fetch('./celeste.gba');
+      const response = await fetch('/celeste.gba');
       if (!response.ok) {
         return;
       }
 
       const data = new Uint8Array(await response.arrayBuffer());
-      const name = writeFile(data, 'gba');
+      const name = write(data, 'gba');
       this.eggvanceLoadGba(name);
     } finally {
       disabled.value = false;
