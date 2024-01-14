248a2ddf6778d2438323062b660624ecc0358df0
jsmolka <jsmolka@pm.me>
2024-01-14 21:13:55 +0100

    modernize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './main.scss';
+import { router } from './router';
 
-const app = createApp(App);
-app.mount('#app');
+function main() {
+  const app = createApp(App);
+  app.use(router);
+  app.mount('#app');
+}
+
+main();
