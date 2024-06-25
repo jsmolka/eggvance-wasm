d1cdb9cbeb45a2593f87bc608dcd107fb6405b3c
jsmolka <jsmolka@pm.me>
2024-06-25 23:36:56 +0200

    update components

--- src/components/ui/select/SelectItem.vue
+++ src/components/ui/select/SelectItem.vue
@@ -3,17 +3,15 @@
     v-bind="forwardedProps"
     :class="
       cn(
-        'relative flex items-center w-full pl-2 pr-8 py-1.5 cursor-pointer rounded-sm focus:bg-shade-6 focus-visible:ring-0 data-[disabled]:opacity-50 data-[disabled]:pointer-events-none',
+        'relative flex items-center w-full pl-2 pr-8 py-1.5 cursor-pointer rounded-sm focus:bg-shade-6 data-[disabled]:opacity-50 data-[disabled]:pointer-events-none',
         props.class,
       )
     "
   >
-    <span class="absolute right-2 flex justify-center items-center size-3.5">
-      <SelectItemIndicator>
-        <CheckIcon class="size-4 text-shade-3" />
-      </SelectItemIndicator>
-    </span>
     <slot />
+    <SelectItemIndicator as-child>
+      <CheckIcon class="absolute right-2 size-4 text-shade-3" />
+    </SelectItemIndicator>
   </SelectItem>
 </template>
 
